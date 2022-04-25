9583c798092c4d365ddd04ccac60e46c2c99edaa
Maksym Mukanovskyi <maksymmukanovskyi@gmail.com>
2022-04-25 17:06:53 -0600

    added hint functional

--- Basics/Second Home work/index.js
+++ Basics/Second Home work/index.js
@@ -66,7 +66,7 @@
 
 
 
-
+//Constructor
 
 function Question(test){
     this.question = test['1'];
@@ -110,10 +110,12 @@ Question.prototype.checkIfCorrectAnswer = function(){
     return userSelected;
 }*/
 
+//Data
+
 const questions = [
     {
-        1: 'When JS was invented?',
-        2: ['1: 2002','2: 1995','3: 1987'],
+        1: 'When M was born?',
+        2: ['1: 2002','2: 1990','3: 1987'],
         3: 2,
     },
     {
@@ -134,60 +136,209 @@ const questions = [
         3: 1,
     
     },
+    {
+        1: 'Where M was born?',
+        2: ['1: Zimbabve',
+            '2: Ganduras',
+            '3: Sibir'
+        ],
+        3: 1,
+    
+    },
+    {
+        1: 'What is the leve of English M has?',
+        2: ['1: High level',
+            '2: Low level',
+            '3: Middle level'
+        ],
+        3: 3,
+    
+    },
+    {
+        1: 'Name of first love of M?',
+        2: ['1: Frasyna',
+            '2: Dusya',
+            '3: Motra'
+        ],
+        3: 3,
+    
+    },
+    {
+        1: 'Name of M wife?',
+        2: ['1: Alla',
+            '2: Zelepuh',
+            '3: Nastya'
+        ],
+        3: 2,
+    
+    },
+    {
+        1: 'Where M lives?',
+        2: ['1: Japan',
+            '2: Canada',
+            '3: Israel'
+        ],
+        3: 2,
+    
+    },
+    {
+        1: 'Favourite dish of M?',
+        2: ['1: Borshch',
+            '2: Shuba',
+            '3: Shashlyk'
+        ],
+        3: 2,
+    
+    },
+    {
+        1: 'What is the weight of M?',
+        2: ['1: 76',
+            '2: 78',
+            '3: 79'
+        ],
+        3: 2,
+    
+    },
 ]
 
 
-const randomQuestion = function(arrQuestions){
-    const questNum = Math.ceil(Math.random() * (arrQuestions.length - 1 + 1)) - 1;
-    return questNum;
-}
  
+//New Object
 
 const questionsArray = questions.map(el => new Question(el));
 
 
-//     questionsArray[randomNumFromArr].showQuestionWithAnswer();
-// questionsArray[randomNumFromArr].checkIfCorrectAnswer();
 
 
 
 //Selectors
 const questSelector = document.querySelector('.question');
-const answerSelector = Array.from(document.querySelectorAll('label'));
-const randomQuest = Array.from(document.querySelectorAll('input'));
+const answerSelector = Array.from(document.querySelectorAll('span'));
+let labelContent;
+let randomQuest;
 const resultAnsw = document.querySelector('.result');
+const content = document.querySelector('.main');
+const container = document.querySelector('.container');
+const finish = document.querySelector('.finish');
+const score = document.querySelector('.total');
 
+
+//State
 
 const state = {
-    currentObj: questionsArray[randomQuestion(questions)],
-    // htmlData: this.currentObj.showQuestionWithAnswer(),
+    // currentObj: questionsArray[randomQuestion(questions)],
+    currentObj: questionsArray[0],
+
+    correctAnswer : false,
+    currentQuestion: -1,
+    countCorrect: 0,
 }
-console.log(state.currentObj)
+
+
+//Data selection
+
+const randomQuestion = function(arrQuestions){
+    // const questNum = Math.ceil(Math.random() * (arrQuestions.length - 1 + 1)) - 1;
+    // return questNum;
+    
+    if(state.currentQuestion < arrQuestions.length){
+        state.currentQuestion += 1;
+    }
+    console.log(state.currentQuestion);
+    return state.currentQuestion
+}
+
+
+//Controllers
 
 const updateUi = function(){
-    state.currentObj = questionsArray[randomQuestion(questions)];
-    let htmlData = state.currentObj.showQuestionWithAnswer();
-    questSelector.textContent = htmlData[0];
-    for(let i = 0; i< htmlData[1].length; i++){
-    answerSelector[i].textContent = htmlData[1][i];
-}
+    
+    console.log('update ui')
+
+    if(state.currentQuestion < questionsArray.length-1){
+        state.currentObj = questionsArray[randomQuestion(questions)];
+        let htmlData = state.currentObj.showQuestionWithAnswer();
+        questSelector.textContent = htmlData[0];
+    
+    
+        content.innerHTML = htmlData[1].map(el => `<label>
+        <input  data-number="${el.slice(0,1)}" type="radio"  value="${el.slice(0,1)}"/>
+        <span>${el}</span>
+    </label>`).join(' ');
+    
+            
+    
+        labelContent = Array.from(document.querySelectorAll('label'));
+        randomQuest = Array.from(document.querySelectorAll('input'));
+        resultAnsw.textContent = '';
+        container.classList.remove('hidden');
+        container.classList.add('visible');
+    }else{
+        finish.textContent = "GAME OVER";
+        score.textContent = `Your total corect score is: ${state.countCorrect < 1? 'Looser': state.countCorrect} out of ${questionsArray.length}`;
+    }
+    
+
+
+
 };
 
+const checkAnswer = function(){
+    for(let question of randomQuest){
+
+        if(question.checked){
+
+            if( state.currentObj.correAnsw == question.dataset.number){
+                state.correctAnswer = true;
+                state.countCorrect += 1;
+
+            }else{
+                state.correctAnswer = false;
+                labelContent.map(el => el.lastElementChild.textContent.slice(0,1) == state.currentObj.correAnsw?el.style.color = "green": null);
+            }
+        }
+
+    }
+    if(state.correctAnswer){
+        resultAnsw.classList.remove('red');
+        resultAnsw.classList.add('green');
+    }else{
+        resultAnsw.classList.remove('green');
+        resultAnsw.classList.add('red');
+    }
 
 
+  resultAnsw.textContent = `You are ${state.correctAnswer? 'Corect' : 'Wrong'}!`;
+
+
+
+}
 
 const globalController = function (){
 
     randomQuest.map(el => el.addEventListener('click', onAnswer));
-
     function onAnswer(e){
 
         if (e.currentTarget.matches('input')) {
-        updateUi();
-        
+            container.classList.add('hidden');
+
+            checkAnswer();
+
+              setTimeout(() => {
+                updateUi();
+
+        randomQuest.map(el => el.addEventListener('click', onAnswer));
+
+              }, 2500)
     }
-}
 };
+
+
+
+
+};
+
+//Initialization
 
 updateUi();
 globalController();
@@ -197,22 +348,7 @@ globalController();
     
 
 
-    /*const checkAnswer = function self(arr){
-        
-
-
-        const recursionFn  =  self.bind(this);
-        const userInput = inp;
-            const isNumerical = !isNaN(parseFloat(userInput))
-            let typedAnswer = '';
-    
-            if(isNumerical){
-                typedAnswer = userInput;
-            }else{
-                recursionFn(this.askAnsqwer());
-            }
-            return typedAnswer;
-    };*/
+   
     
 
 
